47d3e34d69304a19276544c60b3ec977b05aa127
nyitrai100 <nyitrai100@gmail.com>
2023-09-17 13:56:24 +0100

    construction function

---
+++ web development/Javascript/contruction function/index.js
@@ -0,0 +1,21 @@
+var housekeeper = {
+	yearOfExperience: 12,
+	name: "Jane",
+	cleaningRebertoire: ["bathroom", "lobby", "bedroom"]
+};
+
+function HouseKeeper(yearOfExperience, name, cleaningRebertoire){
+	this.yearOfExperience = yearOfExperience,
+		this.name = name,
+		this.cleaningRebertoire = cleaningRebertoire
+} 
+
+var houseKeeper1 = new HouseKeeper(10, "Alyssa", ["living room", "bedroom"])
+
+function MaintenanceStaff(yearOfExperience, name, expertise){
+	this.yearOfExperience = yearOfExperience,
+		this.name= name,
+		this.expertise = expertise
+}
+
+var maintenanceStaff1 = new MaintenanceStaff(25, "Bill", ["gardening", "electricity"])
