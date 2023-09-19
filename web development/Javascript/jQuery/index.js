b89a4b31ca3e3a2288a112361219b5db1e906112
nyitrai100 <nyitrai100@gmail.com>
2023-09-19 19:22:17 +0100

    .

--- web development/Javascript/jQuery/index.js
+++ web development/Javascript/jQuery/index.js
@@ -7,3 +7,16 @@ $("button").click(function(){
 $("h1").text("Bye")
 $("button").text("Don't click me")
 $("a").attr("href", "https://www.yahoo.com")
+
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+})
+
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "green");
+})
+
+$("h1").before("<button>Before</button>");
+
+$("h1").after("<button>After</button>");
