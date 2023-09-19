7aed81e4f37df2ffe57d17571322eec3dec18f43
nyitrai100 <nyitrai100@gmail.com>
2023-09-19 19:09:24 +0100

    click function

--- web development/Javascript/jQuery/index.js
+++ web development/Javascript/jQuery/index.js
@@ -1,2 +1,9 @@
 
-$("h1").addClass("big-title");
+$("h1").addClass("big-title margin-50");
+$("button").click(function(){
+    $("h1").css("color", "purple");
+})
+
+$("h1").text("Bye")
+$("button").text("Don't click me")
+$("a").attr("href", "https://www.yahoo.com")
