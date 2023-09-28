1c1aca5cf12818d1ba587a8a4ef3ce31ad923dde
nyitrai100 <nyitrai100@gmail.com>
2023-09-28 18:36:57 +0100

    import superheroes npm

--- web development/backend/2.3 NPM/index.js
+++ web development/backend/2.3 NPM/index.js
@@ -1,7 +1,17 @@
 // var generateName = require('sillyname');
 
 
-import generateName from "sillyname";
-var sillyName = generateName();
+// import generateName from "sillyname";
+// var sillyName = generateName();
 
-console.log("My name is " + sillyName);
+// console.log("My name is " + sillyName);
+
+
+// const superheroes = require('superheroes');
+import superheroes from "superheroes";
+ 
+superheroes.all;
+ 
+var superhero = superheroes.random();
+
+console.log(`I am a ${superhero}!`)
