422dc0d388531db40979fb1252b7557fd79d4ed9
nyitrai100 <nyitrai100@gmail.com>
2023-11-19 19:23:03 +0000

    .

--- web development/backend/3.2 HTTP Requests/index.js
+++ web development/backend/3.2 HTTP Requests/index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res)=>{
     res.send("<h1>Hello World</h1>");
 })
 
+app.get("/about", (req, res)=>{
+    res.send("<h1>About page</h1>");
+})
+
+app.get("/contact", (req, res)=>{
+    res.send("<h1>Contact page</h1>");
+})
+
 
 app.listen(port, () =>{
     console.log(`Server is started on port ${port}.`)
